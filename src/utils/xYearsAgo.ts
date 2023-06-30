80b5d63aee8908500deaa086d58f957bae23e702
olinelson <olivernelson@icloud.com>
2023-06-30 17:46:54 +1000

    remove moment

--- src/utils/xYearsAgo.ts
+++ src/utils/xYearsAgo.ts
@@ -1,5 +1,8 @@
-import moment from "moment"
-
 export function xYearsAgo(years: number) {
-  return moment().subtract(years, "years").format("YYYY-MM-DD")
+  const date = new Date()
+  date.setFullYear(date.getFullYear() - years)
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  return `${year}-${month}-${day}`
 }
